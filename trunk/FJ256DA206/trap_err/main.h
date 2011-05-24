21e7d3eeb1daf9d0fa235900c03fb8dbd0b01802
Sav <Sav@5e262ccd-b238-a628-99ef-7987f9eb6444>
2011-05-24 11:29:23 +0000

    Всевозможные исключения процессора

---
+++ trunk/FJ256DA206/trap_err/main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_INCL_
+#define _MAIN_INCL_
+
+#include <p24Fxxxx.h>
+#include <libpic30.h>
+
+#define MAIN_IPL	0	/* Common IPL value */
+
+extern unsigned int __attribute__((persistent)) rst_events;
+extern unsigned int rst_state;
+extern unsigned int mcu_id;
+
+#endif /*_MAIN_INCL_*/
---
+++ trunk/FJ256DA206/trap_err/trap_err.h
@@ -0,0 +1,9 @@
+/*
+*	Generate some traps
+*/
+#ifndef _TRAP_ERR_INCL_
+#define _TRAP_ERR_INCL_
+
+extern void check_traps(void);
+
+#endif /*_TRAP_ERR_INCL_*/
