d3221d4a1d518f931558f7ad3747349a946edab7
Sav <Sav@5e262ccd-b238-a628-99ef-7987f9eb6444>
2011-05-24 11:34:52 +0000

    Удалил добавленные по ошибке копии проектов

--- FJ256DA206/Trap_err/trap_err.h
+++
@@ -1,9 +0,0 @@
-/*
-*	Generate some traps
-*/
-#ifndef _TRAP_ERR_INCL_
-#define _TRAP_ERR_INCL_
-
-extern void check_traps(void);
-
-#endif /*_TRAP_ERR_INCL_*/
--- FJ256DA206/Trap_err/trap_err_.s
+++
@@ -1,8 +0,0 @@
-.global _illegal_opcode_func ; Export
-
-.text
-_illegal_opcode_func:
-nop
-.pbyte 0x00, 0xE4, 0x0B ; Illegal instruction opcode
-nop
-return
