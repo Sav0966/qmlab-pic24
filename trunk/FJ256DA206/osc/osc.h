196e076a653ef1df0c0b154bead7a1b419d022a5
Sav <Sav@5e262ccd-b238-a628-99ef-7987f9eb6444>
2011-07-14 13:53:15 +0000

    Перенес определения частот для разных режимов генератора из osc.h в config.h

--- trunk/FJ256DA206/osc/osc.h
+++ trunk/FJ256DA206/osc/osc.h
@@ -4,45 +4,11 @@
 #ifndef _OSC_INCL_
 #define _OSC_INCL_
 #include "p24Fxxxx.h"
-
-#ifndef FOSC
-#include "fosc.h"	/* Main oscillator frequency */
-#endif
+#include "config.h"
 
 typedef enum tagOSC_MODE { // Valid mode selection values
 	FRC, FRCPLL, PRI, PRIPLL, SOSC, LPRC, FRC16, FRCDIV
 } OSC_MODE;
-/*
-* Definitions of FCY value in different modes
-*/
-#define FCY_FRC		8000000UL
-#define FCY_FRC16	(FCY_FRC/16UL)
-/* Define it for RCDIV = 2 only */
-#define FCY_FRCDIV	(FCY_FRC/2UL)
-#define FCY_FRCPLL	(8UL*FCY_FRCDIV)
-
-#define FCY_LPRC	31000UL
-#define FCY_SOSC	32768UL
-
-#define FCY_PRI	((unsigned long)FOSC)
-
-#if (FOSC > 47990000L) // 48 to 32 MHz
- #define FCY_RPIPLL		((2*FCY_PRI)/3UL)
-#elif (FOSC > 31990000L)  // 32 to 32 MHz
- #define FCY_PRIPLL		FCY_PRI
-#elif (FOSC > 23990000L)  // 24 to 32 MHz
- #define FCY_PRIPLL		((4*FCY_PRI)/3UL)
-#elif (FOSC > 19990000L)  // 20 to 32 MHz
- #define FCY_PRIPLL		((8*FCY_PRI)/5UL)
-#elif (FOSC > 15990000L)  // 16 to 32 MHz
- #define FCY_PRIPLL		(2UL*FCY_PRI)
-#elif (FOSC > 11990000L)  // 12 to 32 MHz
- #define FCY_PRIPLL		((8*FCY_PRI)/3UL)
-#elif (FOSC > 7990000L)   //  8 to 32 MHz
- #define FCY_PRIPLL		(4UL*FCY_PRI)
-#else // For 4 MHz PLL96MHZ clock input
- #define FCY_PRIPLL		(8UL*FCY_PRI)
-#endif
 
 unsigned long get_fcy(OSC_MODE mode);
 /*
