c8e675fc90955da045e0133bb37fe124659ef2c8
sav0966 <sav0966@gmail.com>
2012-04-16 21:10:48 +0000

    Перенес определение симуляции (MPLAB SIM) в отдельный файл _tools.h

--- FJ256DA206/pm/mcu_id.h
+++ FJ256DA206/pm/mcu_id.h
@@ -5,6 +5,7 @@
 #define _MCU_ID_INCL_
 #include <p24Fxxxx.h> /* PIC24F only */
 #include <libpic30.h> /*_prog_addressT */
+#include <_tools.h>
 
 /* Return two words, started from 'addr' */
 unsigned long _mcu_long(_prog_addressT addr);
@@ -15,13 +16,13 @@ unsigned long _mcu_long(_prog_addressT addr);
 #define MCU_CONFIG2		((unsigned int)MCU_CONFIG12)
 #define MCU_CONFIG3		((unsigned int)MCU_CONFIG23)
 
-#if (!defined(__DEBUG) || defined(__MPLAB_DEBUGGER_ICD2))
+#ifdef __MPLAB_SIM /* For MPLAB SIM return 0 */
+ #define MCU_REV	0
+ #define MCU_ID		0
+#else
 extern unsigned long _mcu_revid(void); /* For hardware */
  #define MCU_REV	((unsigned int)(_mcu_revid()>>16))
  #define MCU_ID		((unsigned int)_mcu_revid())
-#else /* For MPLAB SIM and other tools return 0 */
- #define MCU_REV	0
- #define MCU_ID		0
 #endif
 
 #endif /*_MCU_ID_INCL_*/
