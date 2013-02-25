74cfc30a60d4b07fb6d0eff3e46680b8110c6ecc
sav0966@gmail.com <sav0966@gmail.com@5e262ccd-b238-a628-99ef-7987f9eb6444>
2013-02-25 12:31:03 +0000

    Функции работы с последовательными интерфейсом SPI

---
+++ trunk/FJ256DA206/spi/fosc.templ.h
@@ -0,0 +1,4 @@
+#define FOSC	12000000	/* Main oscillator frequency */
+// #define FCY_FRC  8000000UL /* FRC oscillator frequency */
+// #define FCY_LPRC 31000UL /* LPRC oscillator frequency */
+// #define FCY_SOSC 32768UL /* SOSC oscillator frequency */
