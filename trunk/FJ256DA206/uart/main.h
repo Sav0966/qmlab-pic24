9e3914c0fa64ef950c9214890271d99046051092
sav0966@gmail.com <sav0966@gmail.com@5e262ccd-b238-a628-99ef-7987f9eb6444>
2013-02-26 13:36:36 +0000

    Начал переносить обработчики прерывания UART в uart.c

--- trunk/FJ256DA206/uart/main.h
+++ trunk/FJ256DA206/uart/main.h
@@ -1,6 +1,14 @@
 #ifndef _MAIN_INCL_
 #define _MAIN_INCL_
 
+#define BIOS_START_TIME	853801200L	// 20 џэт 1997 15:00:00
+
+#define UART_USED	1 			// Checked URAT module (1-4)
+// You must enable UART1 IO in SIM Simulator Settings Dialog
+// Check 'Enable UART1 IO', 'Rewind input', Output 'Window'
+#define UART_TXBUF_SIZE	32 // Size of Transmitter queue
+#define UART_RXBUF_SIZE	16 // Size of Receiver queue
+
 extern unsigned int rst_state;
 extern unsigned int mcu_id;
 
