1b2156d1203d94f751e0da1730d935eb7e1ad08b
sav0966 <sav0966@gmail.com>
2013-03-21 21:06:29 +0000

    Сменил SPI: 2 - Master, 3 - Slave. Пока добился работы на 4 МГц

--- FJ256DA206/spi/spi_test.c
+++ FJ256DA206/spi/spi_test.c
@@ -5,15 +5,19 @@
 #include <spimui.h>
 #include <spisui.h>
 
-#define SPI_MODE		(S_CKP | S_1000)
+#define SPI_MODE		(S_CKP | S_4000)
 
 #ifndef ARSIZE
 #define ARSIZE(buf) (sizeof(buf)/sizeof(buf[0]))
 #endif
 
 static int stage = 0; // Test stage
+static int cnt, i;
+extern volatile int len;
 
-//static unsigned char _buf[1024];
+unsigned char _buf[1024];
+
+int spi_send(unsigned char* buf, int n);
 
 void spi_test(void)
 { // Called from Main Loop more often than once per 10 ms
@@ -22,32 +26,39 @@ void spi_test(void)
 	 // Once per 0.64 seccond test SPI
 		if (!SPI_IS_INIT(SPI_MASTER)) {
 			SPI_EMASTER_INIT(SPI_MASTER,
-				SPI_MODE, SPI_EN | S_TXI_READY, 2);
+				SPI_MODE, SPI_EN | S_RXI_ANY, 2);
 
-			// Map SPI2 SDI and SCK to SPI1 SDO, SCK pins
-
-			SPI_ESLAVE_INIT(SPI_SLAVE, /* Disable SDO */
-				SPI_MODE | S_DISSDO, SPI_EN | S_RXI_ANY, 2);
+//			SPI_ESLAVE_INIT(SPI_SLAVE, /* Disable SDO */
+//				SPI_MODE, SPI_EN | S_RXI_ANY, 3);
 
 			stage = 0; // Start test
 		}
 	}
 
-	if (!SPI_IS_INIT(SPI_MASTER) ||
-		!SPI_IS_INIT(SPI_SLAVE)) return;
+	if (!SPI_IS_INIT(SPI_MASTER) /*||
+		!SPI_IS_INIT(SPI_SLAVE)*/) return;
+
+if (SPI_TX_COUNT(SPI_MASTER) > 4)
+	__asm__ volatile ("nop\nnop"); // breakpoint
+
 
 	switch(stage) {
 		case 0:
-			SPI_WRITE(SPI_MASTER, 0x9F);
+			{ i = 0; for (; i < 512; i++) _buf[i] = i; }
+			cnt = 0; spi_send(_buf, 512);
+
 			++stage; break; // Next test
 
 		case 1:
-			__asm__ volatile ("nop\nnop\nnop"); // breakpoint
+			{ i = SPI_TX_COUNT(SPI_MASTER);
+					if (i > cnt) cnt = i; } // Save it
+			for (i = 0; len > 0; i++); i *=7;
+			__asm__ volatile ("nop\nnop"); // breakpoint
 			++stage; break; // Next test
 
 		case 2: // Overrun transmitter FIFO
 
-			__asm__ volatile ("nop\nnop\nnop"); // breakpoint
+			__asm__ volatile ("nop\nnop"); // breakpoint
 			++stage; break; // Next test
 
 		case 3: // Try to overrun transmitter buffer
@@ -56,13 +67,13 @@ void spi_test(void)
 
 		case 4: // Receve transmitted string
 
-			__asm__ volatile ("nop\nnop\nnop"); // breakpoint
+			__asm__ volatile ("nop\nnop"); // breakpoint
 			++stage; break; // Next test
 
 		case 5:	++stage; break; // Skip it
 		case 6: // RX buffer must be empty
 
-			__asm__ volatile ("nop\nnop\nnop"); // breakpoint
+			__asm__ volatile ("nop\nnop"); // breakpoint
 			++stage; break; // Next test
 
 		default:
--- FJ256DA206/spi/spimui.h
+++ FJ256DA206/spi/spimui.h
@@ -6,26 +6,6 @@
 
 #include <spis.h>
 /*
-*	Initialize the SPI module for the Standard Master mode of operation
-*/
-#define SPI_MASTER_INIT(n, con, sta, ipl) {\
-	SPI_DISABLE_INT(n); SPI_DISABLE_ERINT(n);\
-	_SPIMD(n) = 0; /* Power on SPI module */\
-\
-	SPISTAT(n) = (sta) & ~SPI_EN; /* Setup RTXI and disable module */\
-	SPICON1(n) = (con) | S_MSTEN; SPICON2(n) = 0; /* Master mode */\
-	SPI_CLR_OERR(n); /* Clear overrun error */\
-\
-	SPI_CLR_FLAG(n); SPI_CLR_ERFLAG(n); /* Clear interrupt flags */	\
-\
-	if (ipl > 0) { /* Enable interrupts, if used */\
-		SPI_SET_IPL(n, ipl); SPI_SET_ERIPL(n, ipl);\
-		SPI_ENABLE_INT(n); SPI_ENABLE_ERINT(n);\
-	}\
-\
-	if ((sta) & SPI_EN) SPI_ENABLE(n);\
-} ((void)0)
-/*
 *	Initialize the SPI module for the Enhanced Master mode of operation
 */
 #define SPI_EMASTER_INIT(n, con, sta, ipl) {\
@@ -45,5 +25,28 @@
 \
 	if ((sta) & SPI_EN) SPI_ENABLE(n);\
 } ((void)0)
+
+#ifdef __not_compile__ // Standard mode is not used
+/*
+*	Initialize the SPI module for the Standard Master mode of operation
+*/
+#define SPI_MASTER_INIT(n, con, sta, ipl) {\
+	SPI_DISABLE_INT(n); SPI_DISABLE_ERINT(n);\
+	_SPIMD(n) = 0; /* Power on SPI module */\
+\
+	SPISTAT(n) = (sta) & ~SPI_EN; /* Setup RTXI and disable module */\
+	SPICON1(n) = (con) | S_MSTEN; SPICON2(n) = 0; /* Master mode */\
+	SPI_CLR_OERR(n); /* Clear overrun error */\
+\
+	SPI_CLR_FLAG(n); SPI_CLR_ERFLAG(n); /* Clear interrupt flags */	\
+\
+	if (ipl > 0) { /* Enable interrupts, if used */\
+		SPI_SET_IPL(n, ipl); SPI_SET_ERIPL(n, ipl);\
+		SPI_ENABLE_INT(n); SPI_ENABLE_ERINT(n);\
+	}\
+\
+	if ((sta) & SPI_EN) SPI_ENABLE(n);\
+} ((void)0)
+#endif //__not_compile__
 
 #endif /*_SPIMUI_INCL_*/
