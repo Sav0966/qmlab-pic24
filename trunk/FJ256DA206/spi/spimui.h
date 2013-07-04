e2827e0d6a0c79a1aadbe4bc3eefc21aaaf84e6b
sav0966@gmail.com <sav0966@gmail.com@5e262ccd-b238-a628-99ef-7987f9eb6444>
2013-07-04 06:11:32 +0000

    Вставил определение инициализации Standatd Master mode

--- trunk/FJ256DA206/spi/spimui.h
+++ trunk/FJ256DA206/spi/spimui.h
@@ -6,28 +6,6 @@
 
 #include <spis.h>
 /*
-*	Initialize the SPI module for the Enhanced Master mode of operation
-*/
-#define SPI_EMASTER_INIT(n, con, sta, ipl) {\
-	SPI_DISABLE_INT(n); SPI_DISABLE_ERINT(n);\
-	_SPIMD(n) = 0; /* Power on SPI module */\
-\
-	SPISTAT(n) = (sta) & ~SPI_EN; /* Setup RTXI and disable module */\
-	SPICON1(n) = (con) | S_MSTEN; SPICON2(n) = 0; /* Master mode */\
-	SPI_CLR_OERR(n); SPI_BUF_ENABLE(n); /* Enable FIFO buffer */\
-\
-	SPI_CLR_FLAG(n); SPI_CLR_ERFLAG(n); /* Clear interrupt flags */	\
-\
-	if (ipl > 0) { /* Enable interrupts, if used */\
-		SPI_SET_IPL(n, ipl); SPI_SET_ERIPL(n, ipl);\
-		SPI_ENABLE_INT(n); SPI_ENABLE_ERINT(n);\
-	}\
-\
-	if ((sta) & SPI_EN) SPI_ENABLE(n);\
-} ((void)0)
-
-#ifdef __not_compile__ // Standard mode is not used
-/*
 *	Initialize the SPI module for the Standard Master mode of operation
 */
 #define SPI_MASTER_INIT(n, con, sta, ipl) {\
@@ -47,6 +25,25 @@
 \
 	if ((sta) & SPI_EN) SPI_ENABLE(n);\
 } ((void)0)
-#endif //__not_compile__
+/*
+*	Initialize the SPI module for the Enhanced Master mode of operation
+*/
+#define SPI_EMASTER_INIT(n, con, sta, ipl) {\
+	SPI_DISABLE_INT(n); SPI_DISABLE_ERINT(n);\
+	_SPIMD(n) = 0; /* Power on SPI module */\
+\
+	SPISTAT(n) = (sta) & ~SPI_EN; /* Setup RTXI and disable module */\
+	SPICON1(n) = (con) | S_MSTEN; SPICON2(n) = 0; /* Master mode */\
+	SPI_CLR_OERR(n); SPI_BUF_ENABLE(n); /* Enable FIFO buffer */\
+\
+	SPI_CLR_FLAG(n); SPI_CLR_ERFLAG(n); /* Clear interrupt flags */	\
+\
+	if (ipl > 0) { /* Enable interrupts, if used */\
+		SPI_SET_IPL(n, ipl); SPI_SET_ERIPL(n, ipl);\
+		SPI_ENABLE_INT(n); SPI_ENABLE_ERINT(n);\
+	}\
+\
+	if ((sta) & SPI_EN) SPI_ENABLE(n);\
+} ((void)0)
 
 #endif /*_SPIMUI_INCL_*/
