ca41dcc44c4af91b0582390900f85aee41405e5e
sav0966@gmail.com <sav0966@gmail.com@5e262ccd-b238-a628-99ef-7987f9eb6444>
2013-07-27 12:17:38 +0000

    Самый простой обработчик дал самый лучший результат: 28% свободного машинного времени при 4 МГц скорости SPI

--- trunk/FJ256DA206/spi/spimui.c
+++ trunk/FJ256DA206/spi/spimui.c
@@ -8,25 +8,10 @@
 
 #include <spimui.h>
 
-unsigned const char *pTX;
-unsigned char *pRX;
+volatile unsigned const char *pTX;
+volatile unsigned char *pRX;
 volatile int len = 0;
 volatile int err = 0;
-
-void SPI_INTFUNC(SPI_MASTER, no_auto_psv)(void)
-{
-	SPI_CLR_FLAG(SPI_MASTER);
-
-	// Internal clock (SCK) only
-	while (SPI_CAN_READ(SPI_MASTER, 1)) {
-	 // Can't read more then have been writen
-		if (len-- > 0) SPI_WRITE(SPI_MASTER, *pTX++);
-		*pRX++ = SPI_READ8(SPI_MASTER);
-	}
-
-//while (SPI_CAN_WRITE(SPI_MASTER))
- //if (len-- > 0) SPI_WRITE(SPI_MASTER, *pTX++);
-}
 
 void SPI_ERR_INTFUNC(SPI_MASTER, no_auto_psv)(void)
 {
@@ -36,18 +21,47 @@ void SPI_ERR_INTFUNC(SPI_MASTER, no_auto_psv)(void)
 	++err;
 }
 
+void SPI_INTFUNC(SPI_MASTER, no_auto_psv)(void)
+{ // Called on RXI_ANY event (ready to read)
+	if (len) // If can read, then can write
+	{ SPI_WRITE(SPI_MASTER, *pTX++); --len; }
+
+	SPI_CLR_FLAG(SPI_MASTER);  // Clear flag and
+	*pRX++ = SPI_READ8(SPI_MASTER); // read byte
+
+//	do { // Do until RX FIFO is not empty:
+		// If can write - write next byte
+//		if (len && SPI_CAN_WRITE(SPI_MASTER))
+//		{ SPI_WRITE(SPI_MASTER, *pTX++); --len; }
+
+		// If can read - read next byte
+//		if (SPI_CAN_READ(SPI_MASTER, 1)) {
+//			SPI_CLR_FLAG(SPI_MASTER);
+//			*pRX++ = SPI_READ8(SPI_MASTER); }
+//	} while (SPI_IS_FLAG(SPI_MASTER));
+
+//if (SPI_TX_COUNT(SPI_MASTER) == 0)
+//	__asm__ volatile ("nop\nnop");
+}
+
 int spi_send(unsigned char* buf, int n)
 {
 	if (len > 0) n = -1; // Can't transmit
 	else {
 		SPI_DISABLE_INT(SPI_MASTER);
-		pTX = pRX = buf; len = n - 4;
-//		if (n > 0) SPI_WRITE(SPI_MASTER, *pTX++);
-SPI_WRITE(SPI_MASTER, *pTX++);
-SPI_WRITE(SPI_MASTER, *pTX++);
-SPI_WRITE(SPI_MASTER, *pTX++);
-SPI_WRITE(SPI_MASTER, *pTX++);
+		pTX = pRX = buf; len = n-8;
+		if (n > 0) 
+//			SPI_SET_FLAG(SPI_MASTER);
+			SPI_WRITE(SPI_MASTER, *pTX++);
 
+SPI_WRITE(SPI_MASTER, *pTX++);
+SPI_WRITE(SPI_MASTER, *pTX++);
+SPI_WRITE(SPI_MASTER, *pTX++);
+SPI_WRITE(SPI_MASTER, *pTX++);
+SPI_WRITE(SPI_MASTER, *pTX++);
+SPI_WRITE(SPI_MASTER, *pTX++);
+SPI_WRITE(SPI_MASTER, *pTX++);
+//SPI_WRITE(SPI_MASTER, *pTX++);
 		SPI_ENABLE_INT(SPI_MASTER);
 	}
 
