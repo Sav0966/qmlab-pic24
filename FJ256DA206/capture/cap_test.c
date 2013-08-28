762da15611d723d1048913eab49ec74d7fd63ad8
sav0966 <sav0966@gmail.com>
2013-08-28 12:35:24 +0000

    Определил основной интерфейс периодомера Пока через #define и глобальные переменные Вынес буфер периодомера во внешний модуль

--- FJ256DA206/capture/cap_test.c
+++ FJ256DA206/capture/cap_test.c
@@ -5,8 +5,12 @@
 #include <clock.h>
 #include <refo.h>
 
+
 #include "pmeter.h"
-DECL_PMETER_UI(IC_USED);
+
+#define BUF_SIZE	1024
+PM_BUFFER(IC_USED, BUF_SIZE+1); // Buffer
+DECL_PMETER_UI(IC_USED); // User interfase
 
 static int i, stage = 0; // Test stage
 
@@ -14,16 +18,13 @@ void cap_test(void)
 { // Called from Main Loop more often than once per 10 ms
 
 	if ((sys_clock() & 0x3F) == 0) {
-	 // Once per 0.64 seccond test SPI
-		if (!IC_IS_INIT(IC_USED)) {
-			IC_INIT(IC_USED, ICT_FCY2 | ICM_DISABLE,
-					0, SYSCLK_IPL+1);
-
-			stage = 0; // Start test
-		}
+	 // Once per 0.64 seccond test period meter
+		PM_INIT(IC_USED, IC_RXI_4DATA, 0, SYSCLK_IPL+1);
+		PM_BUF(IC_USED, BUF_SIZE) = 0xABCD; // Check ptr
+		stage = 0; // Start test
 	}
 
-	if (!IC_IS_INIT(IC_USED)) return;
+	if (!PM_IS_INIT(IC_USED)) return;
 
 	switch(stage) {
 		case 0: // REF frequency
@@ -37,14 +38,13 @@ void cap_test(void)
 
 		case 1: // Run sampling
 
-			INIT_BUF(IC_USED, BUF_SIZE);
-			IC_ENABLE(IC_USED, ICM_RAISE);
+			PM_START(IC_USED, BUF_SIZE, ICM_RAISE);
 
 			++stage; break; // Next test
 
 		case 2: // Wait while buffer is not full
 
-			if (!IS_BUFERR(IC_USED)) break;
+			if (!PM_IS_OBUF(IC_USED)) break;
 
 			++stage; break; // Next test
 
@@ -53,8 +53,8 @@ void cap_test(void)
 
 		case 4: // Stop and nalyse buffer
 
-			ASSERT(!IS_OERR(IC_USED)); // Not overrun FIFO
-			IC_RESET(IC_USED); // Stop sampling
+			ASSERT(!PM_IS_OERR(IC_USED));
+			PM_STOP(IC_USED); // Stop sampling
 			refo_off(); // Turn off REF output
 
 			for (i = 1; i < BUF_SIZE-1; ++i)
@@ -65,11 +65,14 @@ void cap_test(void)
 			ASSERT(((unsigned)_IC_(IC_USED, buf)[i] - // i = BUF_SIZ-1
 						(unsigned)_IC_(IC_USED, buf)[i-1]) != 0x1000);
 
+			// Extra buffer data must be not changed
+			ASSERT(PM_BUF(IC_USED, BUF_SIZE) == 0xABCD);
+
 			__asm__ volatile ("nop\nnop");
 			++stage; break; // Next test
 
-		default: stage = 0;
-			IC_PWOFF(IC_USED);
+		default:
+			PM_DONE(IC_USED);
 			break;
 	} // switch(stage)
 }
--- FJ256DA206/capture/pmeter.h
+++ FJ256DA206/capture/pmeter.h
@@ -1,26 +1,46 @@
 /*
-*	Переидомер с внутрицикловой обработкой (POS-1)
+*	Period meter with Input Capture module
 */
 #ifndef _PMETER_INCL_
 #define _PMETER_INCL_
 
-#include <eds.h>
 #include <caps.h>
+#include <eds.h>
 
-#define BUF_SIZE	1024
+// Use this definition to declare period meter buffer
+#define PM_BUFFER(n, size) __eds__ int _IC_(n, buf)[size]\
+			__attribute__((page, space(eds), noload))
+#define PM_BUF(n, i) _IC_(n, buf)[i]
 
 #define DECL_PMETER_UI(n)\
-	extern __eds__ int _IC_(n, buf)[BUF_SIZE]\
-		__attribute__((page, space(eds), noload));\
 	extern volatile PEINT _IC_(n, pbuf) __attribute__((near));\
 	extern volatile int *_IC_(n, pend) __attribute__((near));\
 	extern volatile int _IC_(n, err) __attribute__((near))
 
-#define INIT_BUF(n, size) /* &buf[0] - &buf[size-2] */\
-	_IC_(n, pbuf).peds = _IC_(n, buf);\
-	_IC_(n, pend) =_IC_(n, pbuf).p.addr + ((size) - 1)
+#define PM_GET_PAGE(n)	(_IC_(n, pbuf).p.page)
 
-#define IS_BUFERR(n)	(_IC_(n, pend) == _IC_(n, pbuf).p.addr)
-#define IS_OERR(n)		(_IC_(n, err)) /* Overrun FIFO */
+#define PM_IS_OBUF(n)	(_IC_(n, pbuf).p.addr >= _IC_(n, pend))
+#define PM_IS_OERR(n)	(_IC_(n, err)) /* Overrun IC FIFO */
+
+#define PM_INIT(n, rxi, sync, ipl)\
+	if (!IC_IS_INIT(n)) /* Init PM with IC @ FCY2 clock */\
+		IC_INIT(n, ICT_FCY2|ICM_DISABLE|(rxi), sync, ipl);\
+	_IC_(n, err) = 0 /* Clear Error */
+
+#define PM_TMAX		(65536 / FCY2) /* 4,096 ms @ 16 MHz */
+
+#define PM_IS_INIT(n)	IC_IS_INIT(n)
+#define PM_DONE(n)		IC_PWOFF(n)
+
+#define PM_START(n, size, mode)\
+	/* From buf[0] to buf[size-2] */\
+	_IC_(n, pbuf).peds = _IC_(n, buf);\
+	_IC_(n, pend) =_IC_(n, pbuf).p.addr + ((size) - 1);\
+	IC_ENABLE(n, mode) /* Enable IC with icm = mode */
+
+#define PM_STOP(n)		IC_RESET(n)
+
+#define PM_PAUSE(n)			IC_DISABLE(n)
+#define PM_RESUME(n, mode)	IC_ENABLE(n, mode)
 
 #endif //_PMETER_INCL_
