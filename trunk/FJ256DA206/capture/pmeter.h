44a7bee49041921f4dd0e88855f05e8cb4e18610
sav0966@gmail.com <sav0966@gmail.com@5e262ccd-b238-a628-99ef-7987f9eb6444>
2013-09-21 12:22:54 +0000

    Начал писать математику (внутрицикловая 2/3)

--- trunk/FJ256DA206/capture/pmeter.h
+++ trunk/FJ256DA206/capture/pmeter.h
@@ -8,14 +8,15 @@
 #include <eds.h>
 
 // Use this definition to declare period meter buffer
-#define PM_BUFFER(n, size) __eds__ int _IC_(n, buf)[size]\
-			__attribute__((page, space(eds), noload))
+#define PM_BUFFER(n, size) __eds__ int _IC_(n, buf)[size + 3]\
+	__attribute__((page, space(eds), noload)) /* +3 bytes */
 #define PM_BUF(n, i) _IC_(n, buf)[i]
 
 #define DECL_PMETER_UI(n)\
 	extern volatile PEINT _IC_(n, pcur) __attribute__((near));\
 	extern volatile int *_IC_(n, pend) __attribute__((near));\
-	extern volatile int _IC_(n, err) __attribute__((near))
+	extern volatile int _IC_(n, err) __attribute__((near));\
+	DECL_PM_MATH_INIT(n); DECL_PM_MATH23(n)
 
 #define PM_GET_PAGE(n)	(_IC_(n, pcur).p.page)
 
@@ -33,14 +34,29 @@
 #define PM_DONE(n)		IC_PWOFF(n)
 
 #define PM_START(n, size, mode)\
-	_IC_(n, pcur).peds = _IC_(n, buf);\
-	/* buf[0] - buf[size-2], last cell is not used */\
+	_IC_(n, pcur).peds = _IC_(n, buf); /* Current pointrr */\
+	/* Using buf[0] - buf[size-2], last cell is not used */\
 	_IC_(n, pend) =_IC_(n, pcur).p.addr + ((size) - 1);\
 	IC_ENABLE(n, mode) /* Enable IC with icm = mode */
 
-#define PM_STOP(n)		IC_RESET(n)
+#define PM_STOP(n)			IC_RESET(n)
 
 #define PM_PAUSE(n)			IC_DISABLE(n)
 #define PM_RESUME(n, mode)	IC_ENABLE(n, mode)
+
+#define PM_IS_RUN(n)		IC_IS_ENABLE(n)
+
+// Methematical functions
+
+#define _PM_MATH_INIT(n)		IC_(n, math_init)
+#define IMPL_PM_MATH_INIT(n)	int _PM_MATH_INIT(n)(void)
+#define DECL_PM_MATH_INIT(n)	extern IMPL_PM_MATH_INIT(n)
+
+#define _PM_MATH23(n)		IC_(n, math_23_task)
+#define IMPL_PM_MATH23(n)	int _PM_MATH23(n)(void)
+#define DECL_PM_MATH23(n)	extern IMPL_PM_MATH23(n)
+
+#define pm_math_init(n)		_PM_MATH_INIT(n)()
+#define pm_math23_task(n)	_PM_MATH23(n)()
 
 #endif //_PMETER_INCL_
