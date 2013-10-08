23e524ca7d5a5e735faa986e7151359e790b83a8
sav0966 <sav0966@gmail.com>
2013-10-08 06:52:05 +0000

    v1.1 Исправил алгоритм отбраковки периодов и сократил время счета Запуск измерения для определения критериев ~ 72 мкс + 5.12 мкс/период, Накопление сумм (с отбраковкой и подсчетом QMC) - около 5.32 мкс/период, расчет общей суммы 20 мкс, вычисление периода - 135 мкс.

--- FJ256DA206/capture/pmvars.h
+++ FJ256DA206/capture/pmvars.h
@@ -20,7 +20,6 @@
 #define _N1		_IC_(IC_USED, N1)
 #define _N2		_IC_(IC_USED, N2)
 #define _N3		_IC_(IC_USED, N3)
-#define _ave	_IC_(IC_USED, ave)
 #define _cmp	_IC_(IC_USED, cmp)
 #define _pQ0	_IC_(IC_USED, pQ0)
 #define _pQ1	_IC_(IC_USED, pQ1)
@@ -28,7 +27,6 @@
 #define _pQ3	_IC_(IC_USED, pQ3)
 #define _T02	_IC_(IC_USED, T02)
 #define _T13	_IC_(IC_USED, T13)
-#define _Tave	_IC_(IC_USED, Tave)
 #define _Sqmc	_IC_(IC_USED, Sqmc)
 
 #if (IC_USED == 1)
@@ -44,9 +42,6 @@ __asm__("	.equiv	_N1, __ic_1_N1		\n"
 		"	.equiv	_dT1, __ic_1_dT1	\n"
 		"	.equiv	_dT2, __ic_1_dT2	\n"
 		"	.equiv	_dT3, __ic_1_dT3	\n");
-//		"	.equiv	_ave, __ic_1_ave	\n"
-//		"	.equiv	_Tave, __ic_1_Tave	\n"
-//		"	.equiv	_ave, __ic_1_cmp	\n");
 #elif (IC_USED == 2)
 __asm__("	.equiv	_N1, __ic_2_N1		\n"
 		"	.equiv	_N2, __ic_2_N2		\n"
@@ -60,9 +55,6 @@ __asm__("	.equiv	_N1, __ic_2_N1		\n"
 		"	.equiv	_dT1, __ic_2_dT1	\n"
 		"	.equiv	_dT2, __ic_2_dT2	\n"
 		"	.equiv	_dT3, __ic_2_dT3	\n");
-//		"	.equiv	_ave, __ic_2_ave	\n"
-//		"	.equiv	_Tave, __ic_2_Tave	\n"
-//		"	.equiv	_ave, __ic_2_cmp	\n");
 #elif (IC_USED == 3)
 __asm__("	.equiv	_N1, __ic_3_N1		\n"
 		"	.equiv	_N2, __ic_3_N2		\n"
@@ -76,9 +68,6 @@ __asm__("	.equiv	_N1, __ic_3_N1		\n"
 		"	.equiv	_dT1, __ic_3_dT1	\n"
 		"	.equiv	_dT2, __ic_3_dT2	\n"
 		"	.equiv	_dT3, __ic_3_dT3	\n");
-//		"	.equiv	_ave, __ic_3_ave	\n"
-//		"	.equiv	_Tave, __ic_3_Tave	\n"
-//		"	.equiv	_ave, __ic_3_cmp	\n");
 #elif (IC_USED == 4)
 __asm__("	.equiv	_N1, __ic_4_N1		\n"
 		"	.equiv	_N2, __ic_4_N2		\n"
@@ -92,9 +81,6 @@ __asm__("	.equiv	_N1, __ic_4_N1		\n"
 		"	.equiv	_dT1, __ic_4_dT1	\n"
 		"	.equiv	_dT2, __ic_4_dT2	\n"
 		"	.equiv	_dT3, __ic_4_dT3	\n");
-//		"	.equiv	_ave, __ic_4_ave	\n"
-//		"	.equiv	_Tave, __ic_4_Tave	\n"
-//		"	.equiv	_ave, __ic_4_cmp	\n");
 #elif (IC_USED == 5)
 __asm__("	.equiv	_N1, __ic_5_N1		\n"
 		"	.equiv	_N2, __ic_5_N2		\n"
@@ -108,9 +94,6 @@ __asm__("	.equiv	_N1, __ic_5_N1		\n"
 		"	.equiv	_dT1, __ic_5_dT1	\n"
 		"	.equiv	_dT2, __ic_5_dT2	\n"
 		"	.equiv	_dT3, __ic_5_dT3	\n");
-//		"	.equiv	_ave, __ic_5_ave	\n"
-//		"	.equiv	_Tave, __ic_5_Tave	\n"
-//		"	.equiv	_ave, __ic_5_cmp	\n");
 #elif (IC_USED == 6)
 __asm__("	.equiv	_N1, __ic_6_N1		\n"
 		"	.equiv	_N2, __ic_6_N2		\n"
@@ -124,9 +107,6 @@ __asm__("	.equiv	_N1, __ic_6_N1		\n"
 		"	.equiv	_dT1, __ic_6_dT1	\n"
 		"	.equiv	_dT2, __ic_6_dT2	\n"
 		"	.equiv	_dT3, __ic_6_dT3	\n");
-//		"	.equiv	_ave, __ic_6_ave	\n"
-//		"	.equiv	_Tave, __ic_6_Tave	\n"
-//		"	.equiv	_ave, __ic_6_cmp	\n");
 #elif (IC_USED == 7)
 __asm__("	.equiv	_N1, __ic_7_N1		\n"
 		"	.equiv	_N2, __ic_7_N2		\n"
@@ -140,9 +120,6 @@ __asm__("	.equiv	_N1, __ic_7_N1		\n"
 		"	.equiv	_dT1, __ic_7_dT1	\n"
 		"	.equiv	_dT2, __ic_7_dT2	\n"
 		"	.equiv	_dT3, __ic_7_dT3	\n");
-//		"	.equiv	_ave, __ic_7_ave	\n"
-//		"	.equiv	_Tave, __ic_7_Tave	\n"
-//		"	.equiv	_ave, __ic_7_cmp	\n");
 #elif (IC_USED == 8)
 __asm__("	.equiv	_N1, __ic_8_N1		\n"
 		"	.equiv	_N2, __ic_8_N2		\n"
@@ -156,9 +133,6 @@ __asm__("	.equiv	_N1, __ic_8_N1		\n"
 		"	.equiv	_dT1, __ic_8_dT1	\n"
 		"	.equiv	_dT2, __ic_8_dT2	\n"
 		"	.equiv	_dT3, __ic_8_dT3	\n");
-//		"	.equiv	_ave, __ic_8_ave	\n"
-//		"	.equiv	_Tave, __ic_8_Tave	\n"
-//		"	.equiv	_ave, __ic_8_cmp	\n");
 #elif (IC_USED == 9)
 __asm__("	.equiv	_N1, __ic_9_N1		\n"
 		"	.equiv	_N2, __ic_9_N2		\n"
@@ -172,9 +146,6 @@ __asm__("	.equiv	_N1, __ic_9_N1		\n"
 		"	.equiv	_dT1, __ic_9_dT1	\n"
 		"	.equiv	_dT2, __ic_9_dT2	\n"
 		"	.equiv	_dT3, __ic_9_dT3	\n");
-//		"	.equiv	_ave, __ic_9_ave	\n"
-//		"	.equiv	_Tave, __ic_9_Tave	\n"
-//		"	.equiv	_ave, __ic_9_cmp	\n");
 #endif // Registers and vars
 
 #endif //_PMVARS_INCL_
