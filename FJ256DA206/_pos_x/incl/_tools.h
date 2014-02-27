4bba4aed37dee115e36592771fc61a23e800810d
sav0966 <sav0966@gmail.com>
2014-02-27 12:10:26 +0000

    Скопировал основные заголовки из тестов. Заголовок pinscfg.h требует редактирования.

---
+++ FJ256DA206/_pos_x/incl/_tools.h
@@ -0,0 +1,75 @@
+/*
+*	MPLAB DEBUGGER tools and useful definitions
+*/
+#ifndef __TOOLS_INCL_
+#define __TOOLS_INCL_
+
+#ifdef __DEBUG
+
+#if (\
+	defined(__MPLAB_DEBUGGER_PK3)		\
+ || defined(__MPLAB_DEBUGGER_ICD2)		\
+ || defined(__MPLAB_DEBUGGER_ICD3)		\
+ || defined(__MPLAB_DEBUGGER_REAL_ICE)	\
+/* || defined(__ADD_YOUR_TOOL_HERE__) */)
+#define __MPLAB_DEBUGGER_TOOL
+#else
+#define __MPLAB_SIM
+#endif
+
+#endif //__DEBUG
+
+#ifdef __DEBUG
+#include <stdio.h>
+#include <stdlib.h>
+
+#define VERIFY(f)		ASSERT(f)
+#define DEBUG_ONLY(f)	(f)
+
+#ifndef	__mkstr__
+#define	__mkstr__(exp)	#exp
+#endif
+
+#define AssertFiledLine(f)	(fprintf(stderr,			\
+	"\nAssertion failed in file %s in line %i: %s\n",		\
+	__FILE__, __LINE__, __mkstr__(f)), 0)
+
+#ifndef __MPLAB_SIM // Stop at the failed line and no messages
+#define ASSERT(f)	if (!(f)) while(1) asm __volatile__("nop\nnop");
+#define TRACE2(sz, p1, p2)
+#define TRACE1(sz, p1)
+#define TRACE(sz)
+#endif //__MPLAB_SIM
+
+#else //__DEBUG
+
+#define VERIFY(f)			((void)(f))
+#define DEBUG_ONLY(f)		((void)0)
+#define TRACE2(sz, p1, p2)
+#define TRACE1(sz, p1)
+#define TRACE(sz)
+#define ASSERT(f)
+
+#endif //__DEBUG
+
+// You must enable UART1 IO in SIM Simulator Settings Dialog
+// Check 'Enable UART1 IO', 'Rewind input', 'Output 'Window'
+#ifndef TRACE
+#define TRACE(sz)			DEBUG_ONLY(fprintf(stderr, "%s", sz))
+#endif
+#ifndef TRACE1
+#define TRACE1(sz, p1)		DEBUG_ONLY(fprintf(stderr, sz, p1))
+#endif
+#ifndef TRACE2
+#define TRACE2(sz, p1, p2)	DEBUG_ONLY(fprintf(stderr, sz, p1, p2))
+#endif
+#ifndef ASSERT
+#define	ASSERT(f) DEBUG_ONLY(\
+	(void)((f) || AssertFiledLine(f) || (abort(), 0)))
+#endif
+
+#ifndef ARSIZE
+#define ARSIZE(ar) (sizeof(ar)/sizeof(ar[0]))
+#endif
+
+#endif /*__TOOLS_INCL_*/
