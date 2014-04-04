0e15d939e41abaf9b9788ecb34f1faa63f873a38
sav0966 <sav0966@gmail.com>
2014-04-04 06:39:06 +0000

    Собираю hex-строки в пакеты с проверкой данных

--- FJ256DA206/_pos_x/loader/hexcmd.c
+++ FJ256DA206/_pos_x/loader/hexcmd.c
@@ -2,16 +2,28 @@
 #include <ctype.h>
 #include "main.h"
 
-void hex_init(PUARTBUF buf) { buf->prog.p.type = -1; }
+void hex_init(PUARTBUF buf)
+{ buf->prog.p.state = PROG_INVALID; }
 
 static int hex2dec(int hex)
 {
 	if (isxdigit(hex)) {
-		toupper(hex); hex -= '0';
+		hex = toupper(hex) - '0';
 		if (hex > 9) hex -= 7;
 	} else hex = -1;
 
 	return(hex);
+}
+
+static int mk_word(unsigned char* str)
+{
+	union {
+		int ret;
+		struct { char lo; char hi; } ch;
+	} u;
+
+	u.ch.hi = *str++; u.ch.lo = *str;
+	return( u.ret );
 }
 
 void hex_command(PUARTBUF buf)
@@ -19,23 +31,48 @@ void hex_command(PUARTBUF buf)
 	int n, sum, dec, bin;
 
 	for (;;) {
-		// ":00000001FF" - min  string length and odd
+		if (buf->prog.p.state == PROG_BUSY)
+		{ // Wait the end of programming
+			__asm__ volatile( /* _flash_helpers doesn't wait */
+			"btsc	NVMCON, #15	\n" /* We wait for it to be */
+			"bra	$-2			"); /* completed (WR == 0) */
+		}
+
+		hex_init(buf); // Reset state (invalid)
+
+		// ':00000001FF' - min of string length (odd)
 		if ((buf->pos < 11) || !(buf->pos & 1)) break;
 
 		for (sum = 0, n = 1; n < buf->pos; n++)
-		{ // Check control sum
+		{ // Move data from RxD buffer to PROG structure
 			if ((dec = hex2dec(buf->rxd[n])) < 0) break;
 
 			if (n & 1) bin = dec << 4;
-			else {		// Byte is read
+			else { // Byte is read
 				bin += dec; sum += bin;
 				buf->prog.b[n/2] = bin;
 			}
 		}
 
-		if ((n != buf->pos) || (sum & 0xFF)) break;
+		// Check control sum
+		if ((n != buf->pos) || (sum & 0xFF)) break; // and length
+		if (n != 2 * buf->prog.p.cb + sizeof("00000001FF")) break;
 
+		// Swap prog.p.addr (prog.b[2] & prog.b[3])
+		buf->prog.p.addr = mk_word(&buf->prog.b[2]);
 
+		// Switch command
+		if (buf->prog.p.type == 1)
+		{ // the end of the file
+
+		} else if (buf->prog.p.type == 4)
+		{ // extended address
+			if (buf->prog.p.cb != 2) break;
+			if (buf->prog.p.addr != 0) break;
+			buf->xaddr = mk_word(&buf->prog.b[5]);
+		} else if (buf->prog.p.type != 0) break;
+
+		buf->prog.p.state = PROG_VALID;
 		return; // OK
 	} // for(;;)
 
